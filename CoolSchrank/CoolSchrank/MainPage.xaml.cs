a1c0a7a1343752402f1018a558bbabe58b029798
AdronTech <markus.goegele@hotmail.com>
2016-11-12 15:56:31 +0100

    Azure Connection

--- CoolSchrank/CoolSchrank/MainPage.xaml.cs
+++ CoolSchrank/CoolSchrank/MainPage.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,9 +25,46 @@ namespace CoolSchrank
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private DeviceClient deviceClient;
+        private DispatcherTimer timer;
+        private Random rnd;
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            deviceClient = DeviceClient.Create("hackaTUMhub", new DeviceAuthenticationWithRegistrySymmetricKey("coolschrank", "Sp8+AaL4QpFn6UAU831M+kHZv1gFDRorMjuS4Skuo4E="));
+
+            rnd = new Random();
+
+            timer.Interval = TimeSpan.FromSeconds(5);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
         }
+
+        private async void Timer_Tick(object sender, object e)
+        {
+
+
+            // Take measurement
+            var temp = rnd.Next(1000);
+
+            // Create a datapoint
+            var telemetryDataPoint = new
+            {
+                id = "coolschrank",
+                temperature = temp,
+                date = DateTime.Now
+            };
+
+            // Format data to a JSON message
+            var json = JsonConvert.SerializeObject(telemetryDataPoint);
+            var message = new Message(Encoding.ASCII.GetBytes(json));
+
+            // Send message to the IoT Hub
+            await deviceClient.SendEventAsync(message);
+        }
+
     }
 }
