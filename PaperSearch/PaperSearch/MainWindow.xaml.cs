825c01839d6cd18a6e809035759801444f6d9f67
AdronTech <markus.goegele@hotmail.com>
2016-11-12 13:21:51 +0100

    Search und IoT Projecte erstellt

---
+++ PaperSearch/PaperSearch/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace PaperSearch
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            String query = textBox.Text;
+
+            Stream response = await ResearchController.MakeRequest(query);
+
+            DataContractJsonSerializer js = new DataContractJsonSerializer(typeof(Spell));
+
+        }
+    }
+}
---
+++ PaperSearch/PaperSearch/ResearchController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Net.Http;
+using System.Web;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PaperSearch
+{
+    public class ResearchController
+    {
+
+        public static async Task<Stream> MakeRequest(String query)
+        {
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "3e2cb50858c44fd9aa7c2b9b8beb4c4c");
+
+            // Request parameters
+            queryString["query"] = query;
+            queryString["complete"] = "0";
+            queryString["count"] = "10";
+            queryString["offset"] = "0";
+            queryString["timeout"] = "1000";
+            queryString["model"] = "latest";
+            var uri = "https://api.projectoxford.ai/academic/v1.0/interpret?" + queryString;
+
+            return await client.GetStreamAsync(uri);
+
+        }
+
+    }
+}
